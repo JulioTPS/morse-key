3523a57dcd29a2f6b863754721e9f7bea7a5e47c
JulioSo <so.julio@hotmail.com>
2025-08-31 17:01:41 -0300

    value indicators

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.addEventListener("keydown", onKeyPress);
-document.addEventListener("keyup", onKeyRelease);
+document.addEventListener("keydown", OnKeyPress);
+document.addEventListener("keyup", OnKeyRelease);
 let audioCtx = new (window.AudioContext ||
   window.webkitAudioContext ||
   window.audioContext)();
@@ -7,17 +7,24 @@ let oscillator;
 let gainNode;
 gainNode = audioCtx.createGain();
 let isPressing = false;
-let completeStop = false;
+let completeStop;
+let slideText;
 const lever = document.getElementById("lever-bar-joint-connection");
 const spring = document.getElementById("spring");
 const volumeDial = document.getElementById("volume");
+const volumeValue = document.getElementById("volume-value");
 let volume = 0.5;
+const dotDurationDial = document.getElementById("dot-duration");
+const dotDurationValue = document.getElementById("dot-duration-value");
+let dotDuration = 150;
 let timer = 0;
 const morseTextMaxSize = 30;
 const morseText = document.getElementById("morse-text");
-document.getElementById("morse-code-sheet").style.fontSize = `${document.getElementById("base").offsetWidth / 80}px`;
+document.getElementById("morse-code-sheet").style.fontSize = `${
+  document.getElementById("base").offsetWidth / 80
+}px`;
 
-function onKeyPress(event) {
+function OnKeyPress(event) {
   if ((event.key === "Control" || event.key === "a") && !isPressing) {
     timer = Date.now();
     lever.classList.remove("raised");
@@ -41,9 +48,9 @@ function onKeyPress(event) {
   }
 }
 
-function onKeyRelease(event) {
+function OnKeyRelease(event) {
   if (event.key === "Control" || event.key === "a") {
-    checkMorseCode(Date.now() - timer);
+    CheckMorseCode(Date.now() - timer);
 
     lever.classList.remove("lowered");
     lever.classList.add("raised");
@@ -63,21 +70,52 @@ function onKeyRelease(event) {
   }
 }
 
-function changeVolume(value) {
+function ChangeVolume(value) {
   volume = value / 100;
+  volumeValue.textContent = volume * 100;
   gainNode.gain.setValueAtTime(volume, audioCtx.currentTime);
 }
 
-function checkMorseCode(duration) {
-  if (duration < 130) {
-    if (morseText.textContent.length >= morseTextMaxSize-1) {
-      morseText.textContent = morseText.textContent.slice(-(morseTextMaxSize-1));
+// The length of a dot is 1 time unit.
+// A dash is 3 time units.
+// The space between symbols (dots and dashes) of the same letter is 1 time unit.
+// The space between letters is 3 time units.
+// The space between words is 7 time units.
+
+function ChangeDotDuration(value) {
+  dotDuration = value;
+  dotDurationValue.textContent = dotDuration;
+}
+
+function CheckMorseCode(duration) {
+  if (duration < dotDuration) {
+    if (morseText.textContent.length >= morseTextMaxSize - 1) {
+      morseText.textContent = morseText.textContent.slice(
+        -(morseTextMaxSize - 1)
+      );
     }
     morseText.textContent += ".";
   } else {
-    if (morseText.textContent.length >= morseTextMaxSize-1) {
-      morseText.textContent = morseText.textContent.slice(-(morseTextMaxSize-1));
+    if (morseText.textContent.length >= morseTextMaxSize - 1) {
+      morseText.textContent = morseText.textContent.slice(
+        -(morseTextMaxSize - 1)
+      );
     }
     morseText.textContent += "-";
   }
+  clearTimeout(slideText);
+  SlideText();
+}
+
+function SlideText() {
+  slideText = setTimeout(() => {
+    if (morseText.textContent.length >= morseTextMaxSize - 1) {
+      morseText.textContent = morseText.textContent.slice(
+        -(morseTextMaxSize - 1)
+      );
+    }
+    morseText.textContent += " ";
+
+    SlideText();
+  }, dotDuration * 7);
 }
